81c1f2a5fef3acec960a7c6db25a7b20dc15364f
Benjamin Bavera <163863090+BenjaminBavera@users.noreply.github.com>
2024-10-09 23:40:26 -0300

    Update Room.java

--- Room.java
+++ Room.java
@@ -18,6 +18,7 @@ public class Room
     private String description;
     private HashMap<String, Room> exits;
     private ArrayList<Objeto> objetos;
+    private ArrayList<Objeto> paraPasar;
     /**
      * Create a room described "description". Initially, it has
      * no exits. "description" is something like "a kitchen" or
@@ -29,6 +30,7 @@ public class Room
         this.description = description;
         exits = new HashMap<>();
         objetos = new ArrayList();
+        paraPasar = new ArrayList();
     }
 
     /**
@@ -85,12 +87,67 @@ public class Room
         return objetosDuplicado;
     }
     
-    public String getObjetos(){
+    public String getNameObjetos(){
         String objetosString = "Hay " + objetos.size() + " objetos: ";
         for (Objeto objeto : objetos){
             objetosString += objeto.getName() + " ";
         }
         return objetosString;
     }
-
+    
+    public ArrayList<Objeto> getObjetos(){
+        return objetos;
+    }
+    
+    public void addParaPasar(Objeto objeto){
+        paraPasar.add(objeto);
+    }
+    
+    public ArrayList<Objeto> getParaPasar(){
+        return paraPasar;
+    }
+    
+    public boolean pertenece(ArrayList<Objeto> lista, Objeto objeto){
+        for (int i = 0; i < lista.size(); i++){
+            if (objeto.getName().equals(lista.get(i).getName())){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean contienenLoMismo(ArrayList<Objeto> lista, ArrayList<Objeto> lista2){
+        if (lista.size() != lista2.size()) return false;
+        boolean contienenLoMismo = false;
+        boolean buscando = true;
+        int i = 0;
+        int ii = 0;
+        while (i < lista.size()){
+            while (ii < lista.size()){
+                if (lista.get(i).getName().equals(lista2.get(ii).getName())){
+                    contienenLoMismo = true;
+                }
+                else {
+                    contienenLoMismo = false;
+                }    
+                ii++;
+            }
+            i++;
+        }
+        return contienenLoMismo;    
+    }
+    
+    public boolean puedePasar(){
+        if (paraPasar.size() == 0) {
+            return true;
+        } else{
+            boolean pasar = false;
+            for (Objeto objeto : objetos){
+                if (pertenece(paraPasar, objeto)){
+                    pasar = true;
+                }
+            }
+            return pasar;
+        }
+    }
 }
